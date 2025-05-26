dfa589bb810e73107547ffa4272624ddefc731e4
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2025-05-27 04:27:17 +0900

    [Fix] video loading cancel / add rl module

--- src/reinforcement-learning/pokerogue-rl.ts
+++ src/reinforcement-learning/pokerogue-rl.ts
@@ -1,25 +1,13 @@
 import { globalScene } from "../global-scene";
-import { LoginPhase } from "../phases/login-phase";
 
 export class PokerogueRL {
   constructor() {}
 
-  skipToNextPhase(currentPhase: string, interval_ms: integer) {
-    return new Promise(resolve => {
-      if (globalScene.currentPhase.constructor.name !== currentPhase) resolve(true);
-      else globalScene.uiInputs.buttonTouch();
-
-      setTimeout(() => {
-        return this.skipToNextPhase(currentPhase, interval_ms);
-      }, interval_ms);
-    });
-  }
-
-  async skipSequence(currentPhase: string, interval_ms: integer) {
-    await this.skipToNextPhase(currentPhase, interval_ms);
-  }
-
-  ab(): void {
-    typeof LoginPhase;
-  }
+    async skipToNextPhase(currentPhase, interval_ms) {
+        while (globalScene.currentPhase.constructor.name === currentPhase) {
+            globalScene.uiInputs.buttonTouch();
+            await new Promise(res => setTimeout(res, interval_ms));
+        }
+        return true;
+    }
 }
